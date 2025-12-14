4e97a2bf99b760549ce6cff354959e79c18d3c01
y-takuro-sudo <y-takuro@webb-official.com>
2025-12-14 14:10:05 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-    　base: '/My-first-portfolio-Site/',
+      // ★ここが修正ポイント！一文字一句正確に合わせました
+      base: '/My-first-portfolio-Site/',
+
       server: {
         port: 3000,
         host: '0.0.0.0',
